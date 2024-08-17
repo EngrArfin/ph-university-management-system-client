0b911387e9d902992fbebfca3d0de5be6a709346
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2024-08-17 13:26:08 +0600

    ph University basic setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import MainLayout from "./components/layout/MainLayout";
+
+function App() {
+  return (
+    <>
+      <h1>ph university management system client 27-1 level-2 part-2</h1>
+      <div>
+        <MainLayout></MainLayout>
+      </div>
+    </>
+  );
+}
+
+export default App;
